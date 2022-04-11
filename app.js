6b739bc4ec2e6022fd70b3691d46f94d783eb29e
Wils Iglesias <wiglesias83@gmail.com>
2022-04-11 19:03:20 +0200

    Config env

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`[OK] Web server listening http://localhost:${port}`);
