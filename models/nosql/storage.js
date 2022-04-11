8a2c43fc12383c6907d9dd3c4792e76614b71060
Wils Iglesias <wiglesias83@gmail.com>
2022-04-11 20:00:01 +0200

    Create Users, Storage and Tracks models

---
+++ models/nosql/storage.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const StorageScheme = new mongoose.Schema(
+  {
+    url: {
+      type: String
+    },
+    filename: {
+      type: Number
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model("storages", StorageScheme);
