b3afda70a34cf27c08b4ee61f49b06f8bfffe93c
Wils Iglesias <wiglesias83@gmail.com>
2022-04-11 19:23:06 +0200

    Install mongoose dependence. Scaffold

---
+++ config/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+  const DB_URI = process.env.DB_URI;
+  mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }, (err, res) => {
+    if(!err) {
+      console.log('*** OK Connection ***')
+    } else {
+      console.log('*** Connection Error ***')
+    }
+  });
+};
+
+module.exports = dbConnect;
