6405ef23ee940ebe0c826c30eaab90ee0ce857d0
Wils Iglesias <wiglesias83@gmail.com>
2022-04-12 01:29:48 +0200

    Validate data

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { getItems, createItem } = require("../controllers/tracks");
+const { validatorCreateItem } = require("../validators/tracks");
 const router = express.Router();
 
 
 router.get("/", getItems);
 
-router.post("/", createItem);
+router.post("/", validatorCreateItem, createItem);
 
 
 module.exports = router;
