b152bb6e67f5abcadab4d40a20cc51787f33958e
Wils Iglesias <wiglesias83@gmail.com>
2022-04-12 00:24:11 +0200

    Refactor upload file. Create storage controller

--- routes/storage.js
+++ routes/storage.js
@@ -1,26 +1,9 @@
-const multer = require("multer");
 const express = require("express");
+const { createItem } = require("../controllers/storage");
+const uploadMiddleware = require("../utils/handleStorage");
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination:function(req, file, cb){
-    const pathStorage = `${__dirname}/../storage`;
 
-    cb(null, pathStorage);
-  },
-  filename:function(req, file, cb){
-    const ext = file.originalname.split(".").pop();
-    const filename = `file-${Date.now()}.${ext}`; // file-123123123.jpg
-
-    cb(null, filename);
-  }
-});
-
-const uploadMiddleware = multer({storage});
-
-router.post("/", uploadMiddleware.single("myfile"), (req, res) => {
-  res.send({a:1});
-});
-
+router.post("/", uploadMiddleware.single("myfile"), createItem);
 
 module.exports = router;
