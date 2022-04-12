67b06676c26bf955ddc7b55106a06ef862fc01ae
Wils Iglesias <wiglesias83@gmail.com>
2022-04-12 21:00:29 +0200

    Create user controller

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const models = {
-  userModel: require('./nosql/users'),
+  usersModel: require('./nosql/users'),
   tracksModel: require('./nosql/tracks'),
   storageModel: require('./nosql/storage'),
 };
