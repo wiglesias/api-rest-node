635f56fe964f041cdca3466d7b35fa364927d717
Wils Iglesias <wiglesias83@gmail.com>
2022-04-12 19:30:51 +0200

    Add delete entry point and fix in storage

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,7 +1,10 @@
+const fs = require("fs");
 const { matchedData } = require("express-validator");
 const { storageModel } = require("../models");
 const { handleHttpError } = require("../utils/handleError");
+
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 
 /**
  * @param {*} req 
@@ -36,23 +39,43 @@ const getItem = async (req, res) => {
  * @param {*} res 
  */
 const createItem = async (req, res) => {
-  const { body, file } = req;
-  console.log(file);
-  const fileData = {
-    filename: file.filename,
-    url: `${PUBLIC_URL}/${file.filename}`
+  try {
+    const { file } = req;
+    const fileData = {
+      filename: file.filename,
+      url: `${PUBLIC_URL}/${file.filename}`
+    };
+    const data = await storageModel.create(fileData);
+    
+    res.send({data});
+  } catch (error) {
+    handleHttpError(res, "ERROR_CREATE_ITEM")
   }
-  const data = await storageModel.create(fileData);
 
-  res.send({data});
 };
-
-const updateItem = async (req, res) => {};
 
 /**
  * @param {*} req 
  * @param {*} res 
  */
-const deleteItem = async (req, res) => {};
+const deleteItem = async (req, res) => {
+  try {
+    const { id } = matchedData(req);
+    const dataFile = await storageModel.findById(id);
+    const deleteResponse = await storageModel.delete({ _id: id });
+    const { filename } = dataFile;
+    const filePath = `${MEDIA_PATH}/${filename}`;
 
-module.exports = { getItems, getItem, createItem, updateItem, deleteItem }
+    // fs.unlinkSync(filePath);
+    const data = {
+      filePath,
+      deleted: deleteResponse.matchedCount,
+    };
+
+    res.send({ data });
+  } catch (e) {
+    handleHttpError(res, "ERROR_DETAIL_ITEMS");
+  }
+};
+
+module.exports = { getItems, getItem, createItem, deleteItem }
