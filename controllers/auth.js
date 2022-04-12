0a15aa26457a973a54fb7b359f5051f3fe3a147b
Wils Iglesias <wiglesias83@gmail.com>
2022-04-13 00:17:29 +0200

    Add login JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,56 @@
 const { matchedData } = require("express-validator");
-const { encrypt } = require("../utils/handlePassword");
+const { encrypt, compare } = require("../utils/handlePassword");
 const { usersModel } = require("../models");
 const { tokenSign } = require("../utils/handleJwt");
+const { handleHttpError } = require("../utils/handleError");
 
 const registerController = async (req, res) => {
-  req = matchedData(req);
-  const password = await encrypt(req.password);
-  const body = {...req, password};
-  const dataUser = await usersModel.create(body);
-  dataUser.set("password", undefined, { strict: false });
+  try {
+    req = matchedData(req);
+    const password = await encrypt(req.password);
+    const body = {...req, password};
+    const dataUser = await usersModel.create(body);
+    dataUser.set("password", undefined, { strict: false });
 
-  const data = {
-    token: await tokenSign(dataUser),
-    user: dataUser
+    const data = {
+      token: await tokenSign(dataUser),
+      user: dataUser
+    }
+
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, "ERROR_REGISTER_USER");
   }
-
-  res.send({ data });
 };
 
-module.exports = { registerController }
+const loginController = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const user = await usersModel.findOne({ email:req.email });
+    if (!user) {
+      handleHttpError(res, "USER_NOT_EXISTS", 404);
+      return;
+    }
+
+    const hashPassword = user.password;
+    const check = await compare(req.password, hashPassword);
+
+    if (!check) {
+      handleHttpError(res, "PASSWORD_INVALID", 401);
+      return;
+    }
+
+    user.set('password', undefined, { strict: false });
+    const data = {
+      token: await tokenSign(user),
+      user
+    }
+
+    res.send({ data });
+
+  } catch (error) {
+    handleHttpError(res, "ERROR_LOGIN_USER");
+  }
+}
+
+module.exports = { registerController, loginController }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerController } = require("../controllers/auth");
-const { validatorRegister } = require("../validators/auth");
+const { registerController, loginController } = require("../controllers/auth");
+const { validatorRegister, validatorLogin } = require("../validators/auth");
 
 router.post("/register", validatorRegister, registerController);
+
+router.post("/login", validatorLogin, loginController);
 
 module.exports = router;
