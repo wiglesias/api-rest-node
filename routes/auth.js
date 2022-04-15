b3b1e9ac60ccca8417e6ca96fa2271a0c5dd840e
Wils Iglesias <wiglesias83@gmail.com>
2022-04-15 13:41:48 +0200

    Swagger JWT protected routes. Implenta docs auth register

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,28 @@ const router = express.Router();
 const { registerController, loginController } = require("../controllers/auth");
 const { validatorRegister, validatorLogin } = require("../validators/auth");
 
+/**
+ * http://localhost:3001/api
+ *
+ * Route register new user
+ * @openapi
+ * /auth/register:
+ *  post:
+ *    tags:
+ *      - auth
+ *    summary: "New user registration"
+ *    description: "This route is to register a new user"
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/authRegister"
+ *    responses:
+ *      '201':
+ *         description: The user is successfully registered
+ *      '403':
+ *         description: Validation error
+ */
 router.post("/register", validatorRegister, registerController);
 
 router.post("/login", validatorLogin, loginController);
